7b4788943b3eff503290feb3684ea57c3b4a0106
Lanh Hoang <lanhhoang@users.noreply.github.com>
2023-04-08 00:11:33 -0400

    Add process env to Vite env

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,13 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: viteEnv,
   plugins: [react()],
+});
+
+const viteEnv = {};
+Object.keys(process.env).forEach((key) => {
+  if (key.startsWith(`VITE_`)) {
+    viteEnv[`import.meta.env.${key}`] = process.env[key];
+  }
 });
